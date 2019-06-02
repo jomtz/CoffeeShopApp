8df327e056e3ab2caa17784e7b020de2cb9cc23a
josuemnez <josue@josuemartinez.com>
2019-06-02 19:36:50 -0400

    Add Variable
    Adding Variable numberOfCoffees

--- app/src/main/java/com/josuemartinez/coffeeshopapp/MainActivity.java
+++ app/src/main/java/com/josuemartinez/coffeeshopapp/MainActivity.java
@@ -16,4 +16,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        int numberOfCoffees = 3;
+        display(numberOfCoffees);
+        displayPrice(numberOfCoffees*5);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
 }
