3d259b1e2e78af87390564badce5979573155192
josuemnez <josue@josuemartinez.com>
2019-06-02 19:01:29 -0400

    Add Quantity and Price
    Add Quantity and Price Methods and Views

--- app/src/main/java/com/josuemartinez/coffeeshopapp/MainActivity.java
+++ app/src/main/java/com/josuemartinez/coffeeshopapp/MainActivity.java
@@ -2,7 +2,13 @@ package com.josuemartinez.coffeeshopapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import java.text.NumberFormat;
 
+/**
+ * This app displays and order form to order coffee.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
