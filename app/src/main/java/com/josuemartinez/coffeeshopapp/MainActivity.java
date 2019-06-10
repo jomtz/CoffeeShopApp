491453ebab3a19b5aec7e9cd59a1ceef90c8f713
josuemnez <josue@josuemartinez.com>
2019-06-10 10:43:12 -0400

    Use Intent
    Create a Intent Email

--- app/src/main/java/com/josuemartinez/coffeeshopapp/MainActivity.java
+++ app/src/main/java/com/josuemartinez/coffeeshopapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.josuemartinez.coffeeshopapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -73,8 +75,14 @@ public class MainActivity extends AppCompatActivity {
 
         int price = calculatePrice(hasWhippedCream, hasChocolate);
         String priceMessage = createOrderSummary(theName, price, hasWhippedCream, hasChocolate);
-        displayMessage(priceMessage);
 
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Coffee Order for " + theName);
+        intent.putExtra(Intent.EXTRA_TEXT,  priceMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
     /**
@@ -111,12 +119,5 @@ public class MainActivity extends AppCompatActivity {
         return priceMessage;
     }
 
-    /**
-     * This method displays the given text on the screen.
-     */
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
-    }
 
 }
